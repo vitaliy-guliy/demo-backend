6bf9fa14de6e3f85dc98dddf1a3a7ce05c2310e1
Maz Arslan <marslan@redhat.com>
2023-01-30 11:10:15 +0000

    simple backend

--- src/main/java/com/redhat/demo/webback/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.demo.webback.repos;
-
-import com.redhat.demo.webback.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
